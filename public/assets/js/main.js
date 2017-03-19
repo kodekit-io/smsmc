f97e4757803e13f6cf184ef6882ed30f31ee8269
Jamal <n.nurjamal@gmail.com>
2017-03-19 16:38:40 +0700

    datetimepicker format

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -3,10 +3,10 @@
         setTimeout(function() { $(".sm-alert a.uk-alert-close").click(); }, 10000);
 
         $('input.datetimepicker[name=startDate]').datetimepicker({
-            format:	'd-m-Y H:i'
+            format:	'd/m/y H:i'
         });
         $('input.datetimepicker[name=endDate]').datetimepicker({
-            format:	'd-m-Y H:i'
+            format:	'd/m/y H:i'
         });
         $('.select-all-keyword').checkAll(
             { container: $('#select-keyword'), showIndeterminate: true }
