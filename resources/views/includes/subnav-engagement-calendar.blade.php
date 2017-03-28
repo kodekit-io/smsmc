fffd70250b2dd084d077491db0d6f58b1b69ced5
Jamal <n.nurjamal@gmail.com>
2017-03-28 16:37:59 +0700

    add favicon

--- resources/views/includes/subnav-engagement-calendar.blade.php
+++ resources/views/includes/subnav-engagement-calendar.blade.php
@@ -1,7 +1,7 @@
 <nav class="uk-navbar-container sm-nav-sub" uk-navbar>
     <div class="uk-navbar-left">
         <ul class="uk-navbar-nav">
-            <li><a href="{!! url('/') !!}/engagement-ticket"><i class="fa fa-ticket fa-fw"></i> Ticket</a></li>
+            <li><a href="{!! url('ticket') !!}t"><i class="fa fa-ticket fa-fw"></i> Ticket</a></li>
             <li><a href="{!! url('/') !!}/engagement-calendar"><i class="fa fa-calendar fa-fw"></i> Calendar</a></li>
             <li><a href="{!! url('/') !!}/engagement-timeline"><i class="fa fa-hashtag fa-fw"></i> Timeline</a></li>
         </ul>
