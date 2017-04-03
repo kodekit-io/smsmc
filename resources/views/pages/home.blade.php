6b212bb0667aa9b14b15ba32c63bf46d969667bc
oglab <n.nurjamal@gmail.com>
2017-04-03 08:31:26 +0700

    home paging ui

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -10,12 +10,22 @@
 @section('content')
     <section class="sm-main uk-container uk-container-expand">
         <div id="projectGrid" class="uk-grid-medium uk-grid-match uk-child-width-1-1 uk-child-width-1-1@s uk-child-width-1-3@m uk-child-width-1-4@xl" uk-grid></div>
+
+        <ul class="uk-pagination uk-flex-center" uk-margin>
+            <li class="uk-disabled"><a href="#"><span uk-pagination-previous></span></a></li>
+            <li class="uk-active"><span>1</span></li>
+            <li><a href="#">2</a></li>
+            <li><a href="#">3</a></li>
+            <li><a href="#">4</a></li>
+            <li><a href="#">5</a></li>
+            <li><a href="#"><span uk-pagination-next></span></a></li>
+        </ul>
     </section>
 
-    <div class="uk-alert-danger sm-alert uk-animation-slide-top-small" uk-alert>
+    {{-- <div class="uk-alert-danger sm-alert uk-animation-slide-top-small" uk-alert>
         <a class="uk-alert-close" uk-close></a>
         <p>Alert notification sample! will automatically hidden after 10 sec</p>
-    </div>
+    </div> --}}
 @endsection
 
 @section('page-level-scripts')
