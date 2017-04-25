3f6a77324a6bcd6fe6f2f9c9e060c26e6ce34f6f
oglab <n.nurjamal@gmail.com>
2017-04-25 16:39:04 +0700

    Ontologi test

--- resources/views/pages/ontologi-test.blade.php
+++ resources/views/pages/ontologi-test.blade.php
@@ -39,7 +39,9 @@
                     if (result[0]===undefined) {
                         $('#'+domId).html(cardEmpty);
                     }
+                    // console.log(result);
                     var result = jQuery.parseJSON(result);
+
                     var chartId = result.chartId;
                     var chartName = result.chartName;
                     var chartInfo = result.chartInfo;
@@ -80,18 +82,39 @@
                             text : '',
                         });
 
-                        var serie=[];
-                        for (var i = 0; i < data.length; i++) {
-    						serie[i] = {
-    	                        name: data[i].name,
-    	                        value: data[i].value,
-    	                        category: data[i].category,
-    	                        node: data[i].node
-    	                    }
-                        }
+                        // var serie=[];
+                        // for (var i = 0; i < data.length; i++) {
+    					// 	serie[i] = {
+    	                //         name: data[i].name,
+    	                //         value: data[i].value,
+    	                //         category: data[i].category,
+    	                //         node: data[i].node
+    	                //     }
+                        // }
+
+
                         var option = {
                             legend: {
-                                data: result.categories,
+                                data: [
+                                    {
+                                        "name": "Pajero"
+                                    },
+                                    {
+                                        "name": "forturner"
+                                    },
+                                    {
+                                        "name": "nissan"
+                                    },
+                                    {
+                                        "name": "cr-v"
+                                    },
+                                    {
+                                        "name": "freed"
+                                    },
+                                    {
+                                        "name": "hr-v"
+                                    }
+                                ],
                                 x: 'left',
                                 y: 'bottom',
                             },
@@ -130,19 +153,290 @@
                                     }
                                 },
                                 draggable: true,
-                                data: result.nodes.map(function (node, idx) {
-                                    node.id = idx;
-                                    return node;
-                                }),
-        						// data: serie,
-                                categories: result.categories,
+                                // json datanya
+        						data: [
+                                    {
+                                        "node": 0,
+                                        "category": 0,
+                                        "name": "Pajero",
+                                        "value": 0
+                                    },
+                                    {
+                                        "node": 1,
+                                        "category": 1,
+                                        "name": "forturner",
+                                        "value": 0
+                                    },
+                                    {
+                                        "node": 2,
+                                        "category": 2,
+                                        "name": "nissan",
+                                        "value": 0
+                                    },
+                                    {
+                                        "node": 3,
+                                        "category": 3,
+                                        "name": "cr-v",
+                                        "value": 21
+                                    },
+                                    {
+                                        "node": 4,
+                                        "category": 4,
+                                        "name": "freed",
+                                        "value": 0
+                                    },
+                                    {
+                                        "node": 5,
+                                        "category": 5,
+                                        "name": "hr-v",
+                                        "value": 21
+                                    },
+                                    {
+                                        "node": 6,
+                                        "category": 3,
+                                        "name": "BandarBola8",
+                                        "value": 1
+                                    },
+                                    {
+                                        "node": 7,
+                                        "category": 3,
+                                        "name": "nyerocosss",
+                                        "value": 2
+                                    },
+                                    {
+                                        "node": 8,
+                                        "category": 3,
+                                        "name": "KanalMedia",
+                                        "value": 4
+                                    },
+                                    {
+                                        "node": 9,
+                                        "category": 3,
+                                        "name": "jailangkun9",
+                                        "value": 2
+                                    },
+                                    {
+                                        "node": 10,
+                                        "category": 3,
+                                        "name": "irengguteng",
+                                        "value": 2
+                                    },
+                                    {
+                                        "node": 11,
+                                        "category": 3,
+                                        "name": "abe_ptm",
+                                        "value": 1
+                                    },
+                                    {
+                                        "node": 12,
+                                        "category": 3,
+                                        "name": "infojatim",
+                                        "value": 6
+                                    },
+                                    {
+                                        "node": 13,
+                                        "category": 3,
+                                        "name": "valinobaen",
+                                        "value": 1
+                                    },
+                                    {
+                                        "node": 14,
+                                        "category": 3,
+                                        "name": "puucing",
+                                        "value": 1
+                                    },
+                                    {
+                                        "node": 15,
+                                        "category": 3,
+                                        "name": "21_sakinah",
+                                        "value": 1
+                                    },
+                                    {
+                                        "node": 16,
+                                        "category": 5,
+                                        "name": "dummyuser1",
+                                        "value": 1
+                                    },
+                                    {
+                                        "node": 17,
+                                        "category": 5,
+                                        "name": "dummyuser2",
+                                        "value": 1
+                                    },
+                                    {
+                                        "node": 18,
+                                        "category": 5,
+                                        "name": "dummyuser3",
+                                        "value": 1
+                                    },
+                                    {
+                                        "node": 19,
+                                        "category": 5,
+                                        "name": "dummyuser4",
+                                        "value": 1
+                                    },
+                                    {
+                                        "node": 20,
+                                        "category": 5,
+                                        "name": "dummyuser5",
+                                        "value": 1
+                                    },
+                                    {
+                                        "node": 21,
+                                        "category": 0,
+                                        "name": "dummyuser6",
+                                        "value": 10
+                                    },
+                                    {
+                                        "node": 22,
+                                        "category": 0,
+                                        "name": "dummyuser7",
+                                        "value": 15
+                                    },
+                                    {
+                                        "node": 23,
+                                        "category": 0,
+                                        "name": "dummyuser8",
+                                        "value": 11
+                                    },
+                                    {
+                                        "node": 24,
+                                        "category": 0,
+                                        "name": "dummyuser9",
+                                        "value": 18
+                                    },
+                                    {
+                                        "node": 25,
+                                        "category": 0,
+                                        "name": "dummyuser10",
+                                        "value": 12
+                                    }
+                                ],
+                                categories: [
+                                    {
+                                        "name": "Pajero"
+                                    },
+                                    {
+                                        "name": "forturner"
+                                    },
+                                    {
+                                        "name": "nissan"
+                                    },
+                                    {
+                                        "name": "cr-v"
+                                    },
+                                    {
+                                        "name": "freed"
+                                    },
+                                    {
+                                        "name": "hr-v"
+                                    }
+                                ],
                                 force: {
-                                    repulsion: 50,
-                                    gravity: 0.1,
-                                    edgeLength: 30,
+                                    repulsion: 25,
+                                    gravity: 0.01,
+                                    edgeLength: 50,
                                     layoutAnimation: false,
                                 },
-                                edges: result.links,
+                                // json untuk linknya
+                                edges: [
+                                    {
+                                        "source": 3,
+                                        "target": 6
+                                    },
+                                    {
+                                        "source": 3,
+                                        "target": 7
+                                    },
+                                    {
+                                        "source": 3,
+                                        "target": 8
+                                    },
+                                    {
+                                        "source": 3,
+                                        "target": 9
+                                    },
+                                    {
+                                        "source": 3,
+                                        "target": 10
+                                    },
+                                    {
+                                        "source": 3,
+                                        "target": 11
+                                    },
+                                    {
+                                        "source": 3,
+                                        "target": 12
+                                    },
+                                    {
+                                        "source": 3,
+                                        "target": 13
+                                    },
+                                    {
+                                        "source": 3,
+                                        "target": 14
+                                    },
+                                    {
+                                        "source": 3,
+                                        "target": 15
+                                    },
+                                    {
+                                        "source": 5,
+                                        "target": 7
+                                    },
+                                    {
+                                        "source": 5,
+                                        "target": 8
+                                    },
+                                    {
+                                        "source": 5,
+                                        "target": 16
+                                    },
+                                    {
+                                        "source": 5,
+                                        "target": 17
+                                    },
+                                    {
+                                        "source": 5,
+                                        "target": 18
+                                    },
+                                    {
+                                        "source": 5,
+                                        "target": 19
+                                    },
+                                    {
+                                        "source": 5,
+                                        "target": 20
+                                    },
+                                    {
+                                        "source": 0,
+                                        "target": 21
+                                    },
+                                    {
+                                        "source": 0,
+                                        "target": 22
+                                    },
+                                    {
+                                        "source": 0,
+                                        "target": 23
+                                    },
+                                    {
+                                        "source": 0,
+                                        "target": 24
+                                    },
+                                    {
+                                        "source": 0,
+                                        "target": 25
+                                    },
+                                    {
+                                        "source": 3,
+                                        "target": 25
+                                    },
+                                    {
+                                        "source": 5,
+                                        "target": 25
+                                    }
+                                ],
                                 itemStyle: {},
                             }]
 
