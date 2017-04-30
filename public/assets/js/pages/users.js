1974b544553ef45348ba3aa6700c0c33fe068df1
oglab <n.nurjamal@gmail.com>
2017-04-30 10:58:06 +0700

    Modal delete user

--- public/assets/js/pages/users.js
+++ public/assets/js/pages/users.js
@@ -1,6 +1,25 @@
 (function ($, window, document) {
     $(function () {
 		userlist('users', baseUrl + '/setting/user/list');
+
+        // Delete modal
+        $('#users').on('click', '.btn-delete', function(e) {
+            e.preventDefault();
+            $(this).blur();
+            var id = $(this).attr('data-id');
+			var name = $(this).attr('data-name');
+            var url = '/setting/user/' + id + '/';
+            var modal = '<div class="uk-modal-body">' +
+                    '<h5>Are you sure ?</h5>' +
+					'<p>User <strong>'+name+'</strong> will be deleted</p>' +
+                '</div>' +
+                '<div class="uk-modal-footer uk-clearfix">' +
+                    '<a class="uk-modal-close uk-button grey white-text">CANCEL</a>' +
+                    '<a href="' + url + 'delete" class="uk-button uk-float-right red white-text">DELETE</a>' +
+                '</div>' +
+            '</form>';
+            var uikitModal = UIkit.modal.dialog(modal);
+        });
 	});
 
 	function userlist(domId,url) {
@@ -51,6 +70,7 @@
 			                "class": "uk-text-right",
 			                "data": function ( data ) {
 			                    var id = data["idLogin"];
+								var name = data["userName"];
 			                    var url = '/setting/user/' + id + '/';
 			                    var editBtn = '';
 			                    if ($canEdit) {
@@ -58,7 +78,7 @@
 								}
 			                    var deleteBtn = '';
 			                    if ($canDelete) {
-			                    	deleteBtn = '<a href="' + url + 'delete" title="Delete User" class="uk-button uk-button-small uk-button-default">Delete</a>';
+			                    	deleteBtn = '<a title="Delete User" class="uk-button uk-button-small uk-button-default btn-delete" data-id="'+id+'" data-name="'+name+'">Delete</a>';
 								}
 			                    return editBtn + ' ' + deleteBtn;
 			                }
