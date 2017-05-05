416140178bfee2e3f06891d9ef3fdb231de72017
pashamahardika <github@pashamahardika.com>
2017-05-05 07:59:15 +0000

    fix Storage class not found

--- app/Http/Controllers/ApiAuthController.php
+++ app/Http/Controllers/ApiAuthController.php
@@ -8,6 +8,7 @@ use App\Service\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 
 class ApiAuthController extends Controller
 {
