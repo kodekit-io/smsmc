a659f4dccd885540509c77874088abf96eb66bfc
oglab <n.nurjamal@gmail.com>
2017-05-06 15:33:46 +0700

    Timer page report

--- resources/views/pages/timer-redirect.blade.php
+++ resources/views/pages/timer-redirect.blade.php
@@ -1,23 +1,35 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Sinar Mas Social Media Center</title>
+        <title>Sinar Mas Social Media Center</title>
 
-    <link href="{!! asset('assets/img/favicon.png') !!}" rel="shortcut icon">
+        <link href="{!! asset('assets/img/favicon.png') !!}" rel="shortcut icon">
 
-    <link rel="stylesheet" href="{!! asset('assets/css/main.css') !!}" />
-    @section('page-level-styles')
-    @show
-    <script src="{!! asset('assets/js/lib/jquery.min.js') !!}"></script>
-</head>
-<body>
-<script>
-    function redirect(){
-        this.location.href = '{!! $nextUrl !!}';
-    }
-    setTimeout(redirect, {!! $timeOut !!});
-</script>
+        <link rel="stylesheet" href="{!! asset('assets/css/lib/uikit.css') !!}" />
+        <link rel="stylesheet" href="{!! asset('assets/css/main.css') !!}" />
+		@section('page-level-styles')
+	    @show
+        <script src="{!! asset('assets/js/lib/jquery.min.js') !!}"></script>
+        <script src="{!! asset('assets/js/lib/uikit.js') !!}"></script>
+        <script src="{!! asset('assets/js/lib/uikit-icons.js') !!}"></script>
+        <!-- HTML5 Shiv and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <div class="uk-position-center" uk-spinner></div>
+        <script>
+            function redirect(){
+                this.location.href = '{!! $nextUrl !!}';
+            }
+            setTimeout(redirect, {!! $timeOut !!});
+        </script>
+    </body>
+</html>
