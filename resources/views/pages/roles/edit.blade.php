5e145b37d736d723d336afaabbbd5c4f96e9b3c0
kleur-id <kleur.id@gmail.com>
2017-06-23 15:14:56 +0700

    SEND -> SAVE

--- resources/views/pages/roles/edit.blade.php
+++ resources/views/pages/roles/edit.blade.php
@@ -31,7 +31,7 @@
                     </div>
                     <div class="uk-flex uk-flex-between">
                         <a class="uk-modal-close uk-button grey white-text" href="{!! url('setting/role') !!}">CANCEL</a>
-                        <input type="submit" class="uk-modal-close uk-button uk-float-right red white-text" value="SEND" />
+                        <input type="submit" class="uk-modal-close uk-button uk-float-right red white-text" value="SAVE" />
                     </div>
                 </form>
             </div>
